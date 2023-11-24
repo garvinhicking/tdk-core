9cb58bcc6033387aad3fb5158bacb3ce9892eb79
Garvin Hicking <gh@faktor-e.de>
2023-11-24 15:07:55 +0100

    [TASK] Refactor Plugin

--- src/Plugin.php
+++ src/Plugin.php
@@ -15,12 +15,17 @@ use Composer\Script\Event;
 use Composer\Util\Filesystem;
 use Composer\Script\ScriptEvents;
 use GarvinHicking\TdkCore\Command\CommandProvider;
+use Symfony\Component\Finder\Finder;
 
 final class Plugin implements PluginInterface, EventSubscriberInterface
 {
     protected IOInterface $io;
 
-    public static function getSubscribedEvents()
+    protected string $REPOSITORY_KEY = 'typo3-core';
+    protected string $REPOSITORY_DIR = 'typo3-core';
+    protected bool $initSuccess = false;
+
+    public static function getSubscribedEvents(): array
     {
         return [
             ScriptEvents::PRE_AUTOLOAD_DUMP => ['listen'],
@@ -32,48 +37,119 @@ final class Plugin implements PluginInterface, EventSubscriberInterface
     {
         $this->io = $io;
 
-        $io->writeError('TDK-CORE: Activate event.');
-        // MAYBE: Only ensure the folder is created, if "typo3-core-packages" is a local path
-        // @todo: This is not ideal, but in case the repository is defined but
-        //      the folder does not exist, composer simply breaks.
-        //      This way we achieve a "always working" state.
-//        if($composer->getConfig()->getRepositories()['typo3-core-packages'] ?? false) {
-//            $fs = new Filesystem();
-//            $fs->ensureDirectoryExists('typo3-core/typo3/sysext');
-//        }
-
         $composer->getEventDispatcher()->addSubscriber($this);
     }
 
-    public function listen(Event $event)
+    public function listen(Event $event): void
     {
         if ($event->getName() === ScriptEvents::POST_AUTOLOAD_DUMP) {
-            $event->getIO()->writeError('TDK-Core: listen[post_autoload_dump] event');
+            // Somehow our post_autoload_dump event gets executed twice.
+            // TODO: Find out why, remove this hack.
+            if ($this->initSuccess) {
+                return;
+            }
+            $this->initSuccess = true;
+
+            $this->initializeTYPO3Repository($event);
         }
     }
 
-    public function getCapabilities(): array
+    /**
+     * This is the vital logic of the plugin. This hook allows our plugin
+     * to inject a custom repository to the root composer.json. This can
+     * only happen AFTER the typo3-core has been actually checked out,
+     * because if the repository does not exist, executing "composer install"
+     * would fail.
+     *
+     * Once the plugin has utilized the tdk-cli package and its provided
+     * scripts to GIT checkout the `typo3-core` directory, a follow-up
+     * `composer install` can be issued to actually setup all packages
+     * provided for the core through the local GIT-working directory (and NOT
+     * packagist).
+     *
+     * This allows us to operate via GIT on the typo3-core subdirectory
+     * with more flexibility, and the ability to edit/commit files in there,
+     * cherry-pick other things.
+     *
+     * And it allows us to utilize the tdk-cli and tdk-core to any other
+     * projects, so that a
+     *
+     * @param Event $event
+     * @return bool
+     */
+    protected function initializeTYPO3Repository(Event $event): bool
     {
-        return [
-            CommandProviderCapability::class => CommandProvider::class
-        ];
+        // Check the project's composer.json and see if our repository is listed.
+        $existingRepositories = $event->getComposer()->getConfig()->getRepositories();
+        if (!isset($existingRepositories[$this->REPOSITORY_KEY])) {
+            // Repository missing, our plugin cannot take action.
+            $event->getIO()->writeError(sprintf("TDK: Missing repository <error>%s</error>. TYPO3 cannot be set-up.", $this->REPOSITORY_KEY));
+            return false;
+        }
+
+        // Check if base directory for the GIT checkout is provided...
+        $projectBaseDir = $event->getComposer()->getConfig()->get('vendor-dir') . '/../';
+        if (!is_dir($projectBaseDir . $this->REPOSITORY_DIR)) {
+            $event->getIO()->writeError(sprintf("TDK: Missing directory <error>%s</error>. TYPO3 cannot be set-up.", $this->REPOSITORY_DIR));
+            return false;
+        }
+
+        $gitTargetDir = $projectBaseDir . $this->REPOSITORY_DIR;
+        // Check if it maybe already is a TYPO3 git repository
+        if (file_exists($gitTargetDir . '/.gitignore')) {
+            $event->getIO()->writeError(sprintf("TDK: Directory <info>%s</info> has active TYPO3 set-up.", $this->REPOSITORY_DIR));
+            return false;
+        }
+
+        // If the workflow continues until here, it means that:
+        // * We have a repository key for typo3-core/
+        // * The directory typo3-core/ exists
+        // * There is no .gitignore entry
+        // That means, now we have to make sure that the directory
+        // can be overwritten with a GIT-clone working directory.
+        // To be really safe, we check that NOTHING else besides
+        // our "composer-repository.bak" file exists inside the directory.
+        $finder = Finder::create()
+            ->ignoreVCS(false)
+            ->ignoreDotFiles(false)
+            ->depth(0)
+            ->notName(['composer-repository.bak', '.DS_Store'])
+            ->in($gitTargetDir);
+
+        if (count($finder) > 0) {
+            $event->getIO()->writeError(sprintf("TDK: Directory <info>%s</info> is populated with unknown files. Please reset directory to vanilla state.", $this->REPOSITORY_DIR));
+            return false;
+        }
+
+        // If we have reached this place, we can finally create the GIT checkout.
+        // \GarvinHicking\TdkCli\Cli::help($event);
+        $process = new ProcessExecutor();
+        $command = sprintf('vendor/bin/tdk-cli clone');
+        $event->getIO()->writeError('Create sub-process <info>vendor/bin/tdk-cli clone</info>');
+        $status = $process->executeTty($command);
+
+        if ($status) {
+            $event->getIO()->writeError('<error>Sub-Process returned failure.</error>');
+            return false;
+        }
+
+        $event->getIO()->writeError('<info>Repository cloned. You can now run composer install to activate the extensions!</info>');
+
+        return true;
     }
 
     public function deactivate(Composer $composer, IOInterface $io): void
     {
-        $io->writeError('TDK-CORE: De-Activate event.');
         // TODO: Implement deactivate() method.
     }
 
     public function uninstall(Composer $composer, IOInterface $io): void
     {
-        $io->writeError('TDK-CORE: UnInstall event.');
         // TODO: Implement uninstall() method.
     }
 
     public function install(Composer $composer, IOInterface $io): void
     {
-        $io->writeError('TDK-CORE: Install event.');
         // TODO: Implement install() method.
     }
 
