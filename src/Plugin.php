1f60c9a83a9e85d662eda154d9f5497ec7f504e6
Garvin Hicking <gh@faktor-e.de>
2023-11-24 11:55:12 +0100

    [BUGFIX] Ignore vendor-dir, fix io error

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,23 +5,34 @@ declare(strict_types=1);
 namespace GarvinHicking\TdkCore;
 
 use Composer\Composer;
+use Composer\Config;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\Capability\CommandProvider as CommandProviderCapability;
 use Composer\Plugin\Capable as CapableInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
 use Composer\Util\Filesystem;
+use Composer\Script\ScriptEvents;
 use GarvinHicking\TdkCore\Command\CommandProvider;
 
 final class Plugin implements PluginInterface, CapableInterface, EventSubscriberInterface
 {
     protected IOInterface $io;
 
+    public static function getSubscribedEvents()
+    {
+        return [
+            ScriptEvents::PRE_AUTOLOAD_DUMP => ['listen'],
+            ScriptEvents::POST_AUTOLOAD_DUMP => ['listen'],
+        ];
+    }
+
     public function activate(Composer $composer, IOInterface $io): void
     {
         $this->io = $io;
 
-        $io->info('TDK-CORE: Activate event.');
+        $io->writeError('TDK-CORE: Activate event.');
         // MAYBE: Only ensure the folder is created, if "typo3-core-packages" is a local path
         // @todo: This is not ideal, but in case the repository is defined but
         //      the folder does not exist, composer simply breaks.
@@ -30,11 +41,15 @@ final class Plugin implements PluginInterface, CapableInterface, EventSubscriber
 //            $fs = new Filesystem();
 //            $fs->ensureDirectoryExists('typo3-core/typo3/sysext');
 //        }
+
+        $composer->getEventDispatcher()->addSubscriber($this);
     }
 
-    public static function getSubscribedEvents(): array
+    public function listen(Event $event)
     {
-        return [];
+        if ($event->getName() === ScriptEvents::POST_AUTOLOAD_DUMP) {
+            $event->getIO()->writeError('TDK-Core: listen[post_autoload_dump] event');
+        }
     }
 
     public function getCapabilities(): array
@@ -46,19 +61,28 @@ final class Plugin implements PluginInterface, CapableInterface, EventSubscriber
 
     public function deactivate(Composer $composer, IOInterface $io): void
     {
-        $io->info('TDK-CORE: De-Activate event.');
+        $io->writeError('TDK-CORE: De-Activate event.');
         // TODO: Implement deactivate() method.
     }
 
     public function uninstall(Composer $composer, IOInterface $io): void
     {
-        $io->info('TDK-CORE: UnInstall event.');
+        $io->writeError('TDK-CORE: UnInstall event.');
         // TODO: Implement uninstall() method.
     }
 
     public function install(Composer $composer, IOInterface $io): void
     {
-        $io->info('TDK-CORE: Install event.');
+        $io->writeError('TDK-CORE: Install event.');
         // TODO: Implement install() method.
+    }
+
+    protected static function extractBaseDir(Config $config)
+    {
+        $reflectionClass = new \ReflectionClass($config);
+        $reflectionProperty = $reflectionClass->getProperty('baseDir');
+        /** @noinspection PhpExpressionResultUnusedInspection */
+        $reflectionProperty->setAccessible(true);
+        return $reflectionProperty->getValue($config);
     }
 }
